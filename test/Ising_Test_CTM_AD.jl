110ec8a7661fc896ff7b1ec2a16d47e98c5cdb14
fuyangfeng <386065230@qq.com>
2022-08-31 12:06:57 +0800

    ad

--- test/Ising_Test_CTM_AD.jl
+++ test/Ising_Test_CTM_AD.jl
@@ -135,7 +135,7 @@ end
 function test()
     tem =  vcat(collect(2:0.005:2.268),collect(2.269:0.001:2.27),collect(2.271:0.005:2.5))
     for i in tem
-        @time main(i,80)
+        @time main(i,80,10)
     end
 end
 test()
